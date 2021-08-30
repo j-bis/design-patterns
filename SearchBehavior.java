fe9a8b044d902a9148ed00c9457574e266bf79cc
j-bis <jjtupacz@gmail.com>
2021-08-29 23:52:37 -0400

    manual upload from eclipse

---
+++ SearchBehavior.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+/*
+ * Interface for the 2 types of search
+ * @author Edward Tupacz
+ * */
+public interface SearchBehavior {
+
+	/*
+	 * basic search command
+	 * @param data the arraylist to look through, item the item to look for
+	 * */
+    public static boolean contains(ArrayList<String> data, String item) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+    //doing it wrong when i realized how to properly use an interface
+    /*
+    public int searchmode = 0;
+    public boolean contains(ArrayList<String> data, String item) {
+        if (searchmode = 0;) {
+            return LinearSearch.contains(data, item);
+        }
+        else {
+            return BinarySearch.contains(data, item);
+        }
+    }*/
+}
