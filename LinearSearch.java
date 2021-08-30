7846c3a56fa984435dbd3fdc204e6ee40388b7b9
j-bis <jjtupacz@gmail.com>
2021-08-29 23:53:37 -0400

    manual upload from eclipse
    had these made in vscode, couldnt compile so finished in eclipse

---
+++ LinearSearch.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+/*
+ * default search that goes through the array list one item at a time linearly
+ * @author Edward Tupacz
+ * */
+public class LinearSearch implements SearchBehavior {
+	
+	/*
+	 * search through the list one item at a time
+	 * @param data the arraylist to search through, item the String to look for
+	 * @return true if item is found, false if not
+	 * */
+    public static boolean contains(ArrayList<String> data, String item) {
+        for (int i=0;i<data.size();i++) {
+            if (item.equalsIgnoreCase(data.get(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
