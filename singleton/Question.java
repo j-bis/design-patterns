71ac4110092eab6e6039cebf2677aa27f28863c8
Sobasshi <jjtupacz@gmail.com>
2021-10-04 11:06:15 -0400

    hey quinn theres an output.png

--- singleton/Question.java
+++ singleton/Question.java
@@ -1,11 +1,22 @@
 package singleton;
-
+/**
+ * questions are made here its a factory
+ * @author Edward Tupacz
+ */
 public class Question {
 
     private String question;
     private String[] answers = new String[4];
     private int correctAnswer;
-
+    /**
+     * constructor sets inputs to this instance
+     * @param question the questions being asked
+     * @param ans1 first choice
+     * @param ans2 second choice
+     * @param ans3 thrird chioce
+     * @param ans4 last choice
+     * @param correctAnswer the one in the array thats right
+     */
     public Question(String question, String ans1, String ans2, String ans3, String ans4, int correctAnswer) {
         this.question = question;
         this.correctAnswer = correctAnswer;
@@ -14,17 +25,27 @@ public class Question {
         this.answers[2] = ans3;
         this.answers[3] = ans4;
     }
-
+    /**
+     * prings out the querestions no getters in the uml so i didnt bother with them
+     */
     public String toString() {
         return this.question + "\n1. " + this.answers[0]
         + "\n2. " + this.answers[1] + "\n3. " + this.answers[2]
         + "\n4. " + this.answers[3];
     }
-
+    /**
+     * asks if its correct (you are wrong) 
+     * returns true if you are rigth (you are not)
+     * @param userAnswer the input
+     * @return false
+     */
     public boolean isCorrect(int userAnswer) {
         return this.correctAnswer == userAnswer-1;
     }
-
+    /**
+     * wait there is a getter i forgor abou tthis one it returns instance var
+     * @return the right answer
+     */
     public String getCorrectAnswer() {
         return this.answers[this.correctAnswer];
     }
--- singleton/TriviaGame.java
+++ singleton/TriviaGame.java
@@ -1,28 +1,44 @@
 package singleton;
 import java.util.*;
-
+/**
+ * prepare for bad javadoc
+ * This class creates the game and plays thorugh the trivia game 
+ * pulls from class question and plays with instances of it
+ * @author Edward Tupacz
+ */
 public class TriviaGame {
     private static TriviaGame triviagame;
     private int score;
     private Random rand;
     private Scanner reader;
-    private ArrayList<Question> questions = new ArrayList<Question>();
+    private ArrayList<Question> questions;// = new ArrayList<Question>();
     private int r;
-
+    /**
+     * private constructor takes dataloader stuff and puts it in questions
+     * also sets stuff to this instance and introduces to game
+     */
     private TriviaGame() {
+        questions = new ArrayList<Question>();
         this.rand = new Random();
         this.reader = new Scanner(System.in);
         System.out.println("Welcome to a trivia game");
-        DataLoader.getTriviaQuestions();
+        questions = DataLoader.getTriviaQuestions();
     }
-
+    /**
+     * makes a new triviagmae if one doesnt exist yet and returns it
+     * @return sends this insatance of trivia game and makes new if not one
+     */
     public static TriviaGame getInstance() {
         if (triviagame == null) {
             triviagame = new TriviaGame();
         }
         return triviagame;
     }
-
+    /**
+     * plays through the game, starts with calling playround to do the round
+     * but then does the score calculation and text responses
+     * also while loop does the forever thing until the funny break
+     */
     public void play() {
         String input;
         if (playRound()) {
@@ -30,34 +46,46 @@ public class TriviaGame {
             score++;
         } else {
             System.out.println("You got it wrong!");
-            System.out.println("The correct answer is" + questions.get(r).getCorrectAnswer());
+            System.out.println("The correct answer is " + questions.get(r).getCorrectAnswer());
         }
-        System.out.println("(P)lay or (Q)uit: ");
+        //System.out.println("(P)lay or (Q)uit: ");
         reader.nextLine();
 
         while(true) {
             //input = reader.nextLine();
-            if (reader.nextLine().equalsIgnoreCase("q")) {
+            System.out.println("(P)lay or (Q)uit: ");
+            String playgo = reader.next();
+            if (playgo.equalsIgnoreCase("q")) {
+                System.out.println("you won " + this.score + " games!");
+                System.out.println("BUT you're a quitter so you lose lol");
                 break;
             }
+            reader.nextLine();
             if (playRound()) {
                 System.out.println("YAY! You got it right!");
                 score++;
             } else {
                 System.out.println("You got it wrong!");
-                System.out.println("The correct answer is" + questions.get(r).getCorrectAnswer());
+                System.out.println("The correct answer is " + questions.get(r).getCorrectAnswer());
             }
         }
     }
-
+    /**
+     * plays the round adn gets a random question and asks for your response
+     * @return is your answer correct? i like how i put everything in the return thanks tyler
+     */
     private boolean playRound() {
         this.r = rand.nextInt(10);
         System.out.println(questions.get(r));
         System.out.println("\n\nEnter Answer: ");
-        return questions.get(r).isCorrect(reader.nextInt());
+        //boolean ret = this was for testng ignore
         //reader.nextLine();
+        return questions.get(r).isCorrect(reader.nextInt());
     }
-    
+    /**
+     * i actually didnt need this lol
+     * @param r setting this variable wait maybe i shouldve used this to look better
+     */
     private void setR(int r) {
         this.r = r;
     }
